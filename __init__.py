36458bcb2468e5facc59f7a097cbe468a07ee496
QuanYuan <WeskerYuan@users.noreply.github.com>
2017-09-21 19:56:38 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,32 @@
+#! /usr/bin/python
+"""
+Package for the common source modules.
+
+This package contains the common source modules that will be imported in the 
+main scripts.
+
+Modules:
+    comm.py: this module handles the communication.
+    mas.py: this module has the multi-agent system control algorithm.
+    nav.py: this module has some basic drone navigation and behavior control.
+    util.py: this module contains some utility functions.
+    shared.py: this module has some attributes globally shared among files.
+    
+Copyright:
+    Copyright 2017 Quan Yuan, Adaptive Networks and Control Lab,
+    Research Center of Smart Networks and Systems,
+    School of Information Science and Engineering,
+    Fudan University.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
