9fe964881da336a709d9f291574f6c6c44d631b4
QuanYuan <WeskerYuan@users.noreply.github.com>
2017-09-23 14:05:36 +0800

    Add des_alt disaplay at standby.

--- src/nav.py
+++ src/nav.py
@@ -235,7 +235,7 @@ def _wait_lift_cmd(xbee):
             
         elif wait_count >= 100:
             wait_count = 0
-            comm.xbee_broadcast(xbee,"IFO,%s standby. Alt: %f m." % (shared.AGENT_ID, shared.des_alt))
+            comm.xbee_broadcast(xbee,"IFO,%s standby. Alt: %.2f m." % (shared.AGENT_ID, shared.des_alt))
             
 
 def set_mode(vehicle, mode):
